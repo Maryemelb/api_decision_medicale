08261813c9b8cac150176be8e2d3579b7b2c6d7c
maryem <maryemelbergui@gmail.com>
2025-10-28 15:09:01 +0100

    create server

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-print('salam')
+from fastapi import FastAPI
+app= FastAPI()
+@app.get('/')
+async def index():
+    return "hello"
+@app.get('/prediction')
+async def prediction():
+    return "pred"
