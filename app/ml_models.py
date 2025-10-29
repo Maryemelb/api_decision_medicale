f751ec80524066214e5c4aba82ac3cd9de375cda
maryem <maryemelbergui@gmail.com>
2025-10-29 09:32:40 +0100

    save joblib.dump

--- app/ml_models.py
+++ app/ml_models.py
@@ -6,6 +6,10 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+
 def data_preparation(data):
       data['status']= data['status'].map({'negative':0, 'positive':1})
       return data
@@ -15,23 +19,30 @@ data['status']= data['status'].map({'negative':0, 'positive':1})
 X= data.drop(columns='status')
 y= data['status']
 X_train, X_test, y_train, y_test= train_test_split( X,y, test_size=0.3, random_state=42)
+
 preprocessor= ColumnTransformer(
       transformers=[('X_num',StandardScaler(),X.columns)]
 )
+
 #hyper parametre logistic regression
 hyper_param= {
       'classification__n_estimators': [50,80,100],
       'classification__max_depth': [1,10,100]
 }
-from sklearn.ensemble import RandomForestClassifier
+
 pipeline_lr= Pipeline([
 ('preprocessor',preprocessor),
 ('select_best_features', SelectKBest(score_func=f_classif, k=4)),
 ('classification', RandomForestClassifier())
 ])
+
 greadsearch= GridSearchCV(pipeline_lr,hyper_param, cv=4, scoring='accuracy')
 greadsearch.fit(X_train, y_train)
 y_pred= greadsearch.predict(X_test)
 print('Accuracy: ', metrics.accuracy_score(y_pred, y_test))
 print('recall: ',metrics.recall_score(y_pred, y_test))
 print('f1: ',metrics.f1_score(y_pred, y_test))
+
+best_model= greadsearch.best_estimator_
+# Sauvegarder le modèle dans un fichier
+joblib.dump(best_model, "app/classification_model.pkl")
